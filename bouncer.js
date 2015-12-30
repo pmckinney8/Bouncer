f25ca304888ea475e445595b3f815aec5163756a
oscarvazquez <oscar.vazquez2012@gmail.com>
2015-12-30 09:43:08 -0400

    Made the Bounce function global, and made all the helper methods into local variables. Added credit card and no numbers validation

--- bouncer.js
+++ bouncer.js
@@ -1,144 +1,36 @@
-// input_id = id of the field that you want validated
-// max_length = max length of characters allowed for the input field
-// min_length = min length of characters allowed for the input field
-// capital_letters = input field must contain a capital letter
-// has_numbers = input field must contain a integer
-// email = checks if the email is a valid format
-// no_numbers = input field cannot contain any numbers
-// no_special_characters = input field cannot contain any special characters
-// letters_only = input field can only contain letters
+//input_id
+//max_length
+//min_length
+//require_capital_letters
+//require_
+//has_numbers
+//email
+//no_numbers
+//user_name_check
+//letters_only
 
-$(document).ready(function(){
+///added validation////
 
-  var validation_array = [];
-
-  // LISTENS FOR ANY CHANGES MADE TO ANY INPUT FIELD
-  $('input').keyup(function(){
-  // GRABS THE ID OF THE INPUT FIELD THAT WAS TRIGGERED
-    var id = $(this).attr('id');
-
-  // FOR LOOP THAT CHECKS IF THAT INPUT FIELD WAS IN OUR ARRAY
-    var check_array = [];
-    for(var i = 0; i < validation_array.length; i++){
-
-      if(id == validation_array[i]['input_id']){
-        // CHECK FOR EMAIL
-        if(validation_array[i]['input_id'] == 'email'){
-            var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if(regEx.test($(this).val())){
-              success($(this));
-              return;
-            }else{
-              fail($(this));
-              return;
-            }
-        }
-
-        // MIN LENGTH CHECK
-        if(validation_array[i]['min_length']){
-          var word = $(this).val();
-          if(word.length < validation_array[i]['min_length']){
-            check_array.push('fail');
-          }else if(word.length >= validation_array[i]['min_length']){
-            check_array.push('success');
-          }
-        }
-
-        // MAX LENGTH CHECK
-        if(validation_array[i]['max_length']){
-          var word = $(this).val();
-          if(word.length > validation_array[i]['max_length']){
-            check_array.push('fail');
-          }else if(word.length <= validation_array[i]['max_length']){
-            check_array.push('success');
-          }
-        }
-
-        // MUST HAVE CAPITAL LETTER CHECK
-        if(validation_array[i]['capital_letters'] == true){
-          var word = $(this).val();
-          var capitals = hasLowerCase(word);
-          if(capitals == true){
-            check_array.push('success');
-          }else{
-            check_array.push('fail');
-          }
-        }
-
-        // MUST HAVE A NUMBER CHECK
-        if(validation_array[i]['has_numbers'] == true){
-          var word = $(this).val();
-          var numbers = hasNumbers(word);
-          console.log('numbers', numbers);
-          if(numbers == true){
-            check_array.push('fail');
-          }else{
-            check_array.push('success');
-          }
-        }
-
-        // MUST ONLY CONTAIN LETTERS CHECK
-        if(validation_array[i]['no_numbers'] == true){
-          var word = $(this).val();
-          var numbers = noNumbers(word);
-          console.log('numbers', numbers);
-          if(numbers == true){
-            check_array.push('success');
-          }else{
-            check_array.push('fail');
-          }
-        }
-
-        //NO SPECIAL CHARACTERS CHECK
-        if(validation_array[i]['no_special_characters'] == true){
-          var word = $(this).val();
-          var check = no_special_characters_validate(word);
-          if(check == true){
-            check_array.push('success');
-          }else{
-            check_array.push('fail');
-          }
-        }
-
-        //STRING CAN ONLY BE LETTERS
-        if(validation_array[i]['letters_only'] == true){
-          var word = $(this).val();
-          var check = letters_only_check(word);
-          if(check == true){
-            check_array.push('success');
-          }else {
-            check_array.push('fail');
-          }
-        }
-
-        // SEARCHING THROUGH THE CHECK ARRAY TO SEE IF THERE WERE ANY ERRORS
-        for(var i = 0; i < check_array.length;i++){
-          if(check_array[i] == 'fail'){
-            fail($(this));
-            return;
-          }
-        }
-        // THIS WILL ONLY BE HIT IF THE CHECK_ARRAY DOESNT CONTAIN ANY FALSE STRINGS
-        success($(this));
-        return;
-      }
-    }
-  })
-});
+//credit_card
+//no_letters
+(function(global, $){
 
 
+  var Bouncer = function(validation_array) {
+    return new Bouncer.init(validation_array);
+  }
 
-  function fail($obj) {
+  var fail = function($obj) {
     $obj.removeClass('success');
     $obj.addClass('fail');
   }
 
-  function success($obj) {
+  var success = function($obj) {
     $obj.removeClass('fail');
     $obj.addClass('success');
   }
 
-  function hasLowerCase(str) {
+  var hasLowerCase = function(str) {
       for(x=0;x<str.length;x++){
           if(str.charAt(x) >= 'A' && str.charAt(x) <= 'Z')
               return true;
@@ -146,7 +38,7 @@ $(document).ready(function(){
       return false;
   }
 
-  function hasNumbers(str) {
+  var hasNumbers = function(str) {
     var array = str.split('');
     if(!str.match(/[0-9]/i)){
       return true;
@@ -154,7 +46,7 @@ $(document).ready(function(){
     return false;
   }
 
-  function noNumbers(str) {
+  var noNumbers = function(str) {
     var array = str.split('');
     if(!str.match(/[0-9]/i)){
       return true;
@@ -162,7 +54,17 @@ $(document).ready(function(){
     return false;
   }
 
-  function no_special_characters_validate(str){
+  var noLetters = function(str) {
+    var array = str.split('');
+    if(!str.match(/[a-zA-Z]/i)){
+     return true
+    }else{
+      console.log('failing the test')
+      return false;
+    }
+  }
+
+  var user_name_validate = function(str){
     if(!/[^a-zA-Z0-9_]/.test(str)) {
       return true;
     }else{
@@ -170,11 +72,205 @@ $(document).ready(function(){
     }
   }
 
-
-  function letters_only_check(str){
+  var letters_only_check = function(str){
     if(!/[^a-zA-Z]/.test(str)) {
       return true;
     }else{
       return false;
     }
-}
+  }  
+  var isValidIdentifier = function (identifier) {
+
+      var sum     = 0,
+          alt     = false,
+          i       = identifier.length-1,
+          num;
+
+      if (identifier.length < 13 || identifier.length > 19){
+          return false;
+      }
+
+      while (i >= 0){
+
+          //get the next digit
+          num = parseInt(identifier.charAt(i), 10);
+
+          //if it's not a valid number, abort
+          if (isNaN(num)){
+              return false;
+          }
+
+          //if it's an alternate number...
+          if (alt) {
+              num *= 2;
+              if (num > 9){
+                  num = (num % 10) + 1;
+              }
+          } 
+
+          //flip the alternate bit
+          alt = !alt;
+
+          //add to the rest of the sum
+          sum += num;
+
+          //go to next digit
+          i--;
+      }
+
+      //determine if it's valid
+      return (sum % 10 == 0);
+  }
+
+  Bouncer.prototype = {
+    validate: function(){
+      //ADD INPUT FIELD OBJECTS HERE IF YOU WANT THEM TO BE CHECKED
+      var validation_array = this.validation_array
+
+      // LISTENS FOR ANY CHANGES MADE TO ANY INPUT FIELD
+      $('.bouncer').keyup(function(){
+      // GRABS THE ID OF THE INPUT FIELD THAT WAS TRIGGERED
+        var id = $(this).attr('id');
+      // FOR LOOP THAT CHECKS IF THAT INPUT FIELD WAS IN OUR ARRAY
+        var check_array = [];
+        for(var i = 0; i < validation_array.length; i++){
+          if(id == validation_array[i]['input_id']){
+            // CHECK FOR EMAIL
+
+            // previous I don't understand this line of code
+            // if(validation_array[i]['email'] == true){
+            
+            if(validation_array[i]['input_id'] == 'email'){
+                var regEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if(regEx.test($(this).val())){
+                  success($(this));
+                  return;
+                }else{
+                  fail($(this));
+                  return;
+                }
+            }
+            if(validation_array[i]['input_id'] == 'credit_card'){
+              if(isValidIdentifier($(this).val())){
+                success($(this));
+                return;
+              } else {
+                fail($(this));
+                return;
+              }
+            }
+
+            // MIN LENGTH CHECK
+            if(validation_array[i]['min_length']){
+              var word = $(this).val();
+              if(word.length < validation_array[i]['min_length']){
+                check_array.push('fail');
+              }else if(word.length >= validation_array[i]['min_length']){
+                check_array.push('success');
+              }
+            }
+
+            // MAX LENGTH CHECK
+            if(validation_array[i]['max_length']){
+              var word = $(this).val();
+              if(word.length > validation_array[i]['max_length']){
+                check_array.push('fail');
+              }else if(word.length <= validation_array[i]['max_length']){
+                check_array.push('success');
+              }
+            }
+
+            // MUST HAVE CAPITAL LETTER CHECK
+            if(validation_array[i]['require_capital_letters'] == true){
+              var word = $(this).val();
+              var capitals = hasLowerCase(word);
+              if(capitals == true){
+                check_array.push('success');
+              }else{
+                check_array.push('fail');
+              }
+            }
+
+            // MUST HAVE A NUMBER CHECK
+            if(validation_array[i]['has_numbers'] == true){
+              var word = $(this).val();
+              var numbers = hasNumbers(word);
+              console.log('numbers', numbers);
+              if(numbers == true){
+                check_array.push('fail');
+              }else{
+                check_array.push('success');
+              }
+            }
+
+            //MUST ONLY CONTAIN NUMBERS
+            if(validation_array[i]['no_letters'] == true){
+              var word = $(this).val();
+              var check = noLetters(word);
+              if(check == true){
+                check_array.push('success');
+              }else {
+                check_array.push('fail');
+              }
+            }
+            // MUST ONLY CONTAIN LETTERS CHECK
+            if(validation_array[i]['no_numbers'] == true){
+              var word = $(this).val();
+              var numbers = noNumbers(word);
+              console.log('numbers', numbers);
+              if(numbers == true){
+                check_array.push('success');
+              }else{
+                check_array.push('fail');
+              }
+            }
+
+            //MAKES STANDARD CHECK OF USERNAME FIED
+            if(validation_array[i]['user_name_check'] == true){
+              var word = $(this).val();
+              var check = user_name_validate(word);
+              if(check == true){
+                check_array.push('success');
+              }else{
+                check_array.push('fail');
+              }
+            }
+
+            //STRING CAN ONLY BE LETTERS
+            if(validation_array[i]['letters_only'] == true){
+              var word = $(this).val();
+              var check = letters_only_check(word);
+              if(check == true){
+                check_array.push('success');
+              }else {
+                check_array.push('fail');
+              }
+            }
+
+            // SEARCHING THROUGH THE CHECK ARRAY TO SEE IF THERE WERE ANY ERRORS
+            for(var i = 0; i < check_array.length;i++){
+              if(check_array[i] == 'fail'){
+                fail($(this));
+                return;
+              }
+            }
+            // THIS WILL ONLY BE HIT IF THE CHECK_ARRAY DOESNT CONTAIN ANY FALSE STRINGS
+            success($(this));
+            return;
+          }
+        }
+      })
+    }
+  };
+
+  Bouncer.init = function(validation_array) {
+    var self = this;
+    self.validation_array = validation_array;
+  }
+  
+  Bouncer.init.prototype = Bouncer.prototype;
+
+  global.Bouncer = global.Bounce = Bouncer;
+
+
+}(window, jQuery));
